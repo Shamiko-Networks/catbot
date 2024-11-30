0a1123830f5eefd25d353a2eb4ecdbd8cfadfe9d
HynoR <20227709+HynoR@users.noreply.github.com>
2024-11-30 13:03:53 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build -t catbot .
 docker rm -f chatbot-server
-docker run -d -v /root/cat/chatbot/misc:/app/misc --name chatbot-server catbot
+docker run -d -v /etc/catbot/misc:/app/misc --name chatbot-server catbot
